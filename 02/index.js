db49a6ac3123146c4c2b157e049e1b982f64e8d8
Lucy Suxo <lucy.elaine1@yahoo.com.br>
2023-03-06 03:14:40 -0300

    Atividade 02 finalizada!

---
+++ 02/index.js
@@ -0,0 +1,6 @@
+let fahrenheit = 73;
+let celsius = (fahrenheit - 32) * (5 / 9);
+
+console.log(`Resultado: ${celsius.toFixed()}`);
+
+// toFixed()  -> Arredonda um valor para inteiro quando você não colocar um valor dentro do parentes
